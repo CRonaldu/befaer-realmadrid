91d73291c06e10ee535ecd204e9c051fad662e7c
CRonaldu <fasilpantera@gmail.com>
2026-02-09 17:35:37 +0500

    uppdate

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ class User:
 
     def greet(self):
         return f"Hello, my name is {self.name} and I am {self.age} years old."
+    def unnamed(self , name):
+        self.name = name
     
 user1 = User("Alice", 30)
-print(user1.greet())    
+print(user1.greet())
+user1.unnamed("Bob")
+print(user1.greet())        
 
     
