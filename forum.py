e316b4021ff7c5024eb7893f8baf3a0a3ccb8854
CRonaldu <fasilpantera@gmail.com>
2026-02-10 15:40:46 +0500

    Update forum.py

--- forum.py
+++ forum.py
@@ -24,6 +24,9 @@ class Forum:
 
     def show_comments(self):
         return self.comments
+    
+    def add_comment(self , user , comment):
+        self.comments[user.name] = comment
 
 user1 = User('Dastan' , 18 , 'aktajdastan')
 print (user1.informaton())
